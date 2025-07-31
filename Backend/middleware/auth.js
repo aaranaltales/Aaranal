9c8a198cac4feca513a4e2e78bda2fba79e14802
Yagnesh <yagnesh.glk@gmail.com>
2025-07-31 17:49:33 +0530

    cart implemented

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 const authUser = async (req, res, next) => {
     try {
         const authHeader = req.headers.authorization;
-
+       
         if (!authHeader || !authHeader.startsWith('Bearer ')) {
             return res.status(401).json({ success: false, message: 'Unauthorized: No token provided' });
         }
